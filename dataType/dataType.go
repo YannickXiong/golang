2f3ab8806e5bb1f1118f8a0d078ba1bfc5b98869
YannickXiong <xiong.yannick@student.oaklandcc.edu>
2017-11-01 23:27:21 +0800

    Perfected the part of slice & add Two dimensional array

--- dataType/dataType.go
+++ dataType/dataType.go
@@ -354,6 +354,29 @@ func main() {
 	fmt.Printf("after assign & calc, arr6 => %d, and address of arr6 is %v\n", arr6, &arr6[0])
 	fmt.Printf("after assign & calc, arr7 => %d, and address of arr7 is %v\n\n", arr7, &arr7[0])
 
+	// Two dimensional array
+	arr8 := [4][3]int{{10, 11, 17}, {0, 0, 0}, {20, -9, 4}}
+	fmt.Printf("arr8 is =>%d, len of arr is => %d\n\n", arr8, len(arr8))
+
+	// loop 1
+	for i := 0; i < len(arr8); i++ {
+		for j := 0; j < len(arr8[0]); j++ {
+			fmt.Printf("arr8[%d][%d] => %d, ", i, j, arr8[i][j])
+		}
+		fmt.Printf("\n")
+
+	}
+	fmt.Printf("\n")
+	// loop 2
+
+	for _, subArry := range arr8 {
+		for _, v := range subArry {
+			fmt.Printf("%d ", v)
+		}
+		fmt.Printf("\n")
+	}
+	fmt.Printf("\n")
+
 	/*
 	   Slice: slice is a reference type.
 	   There are 2 ways to create a slice, one is by array, another is by make()
@@ -383,15 +406,76 @@ func main() {
 	s6 := make([]int, 3, 7)
 	fmt.Printf("len of s6 => %d, cap of s6 => %d, s6 =>%d\n", len(s6), cap(s6), s6)
 
+	/*
+		Dilatancy principle: refer to source code of append
+			newcap := old.cap
+		    doublecap := newcap + newcap
+		    if cap > doublecap {
+		        newcap = cap	// how to calc cap ? Refer to s8, s9, s10, s11 among the fellowing test cases.
+		    } else {
+		        if old.len < 1024 {
+		            newcap = doublecap // increase by 2 times
+		        } else {
+		            for newcap < cap {
+		                newcap += newcap / 4  // increase by 1.25 times
+		            }
+		        }
+		    }
+
+	*/
 	// s7 := make([]int{4, 8, 12}, 3, 7), try to init the data when create a slice, will lead to a compile error.
-	s7 := make([]int, 3, 7)
+	s7 := make([]int, 5, 7)
 	fmt.Printf("len of s7 => %d, cap of s7 => %d, s7 =>%d\n", len(s7), cap(s7), s7)
-	// s7 := append(s7, 11, 12, 13, 14, 15) will lead to a compile error.
-	// cap of s8 is 14, doube of s7
-	s8 := append(s7, 11, 12, 13, 14, 15)
+	// s7 := append(s7, 11, 12, 13, 14, 15) will lead to a compile error. Because no new value at left of :=
+	// while s7 = append(s7, 11, 12, 13, 14, 15) is ok
+
+	// cap of s8 is 14, < 2 * 7(cap s6), new cap is 14
+	s8 := append(s6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
 	fmt.Printf("len of s8 => %d, cap of s8 => %d, s8 =>%d\n", len(s8), cap(s8), s8)
 
-	// cap of s9 is 21, three times of s7
-	s9 := append(s7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
+	// cap of s9 is 15, > 2 * 7(cap s6), so new cap is 15 + 1, new cap is always even number, not odd number.
+	s9 := append(s6, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
 	fmt.Printf("len of s9 => %d, cap of s9 => %d, s9 =>%d\n", len(s9), cap(s9), s9)
+
+	// the same, new cap is 16
+	s10 := append(s7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21)
+	fmt.Printf("len of s10 => %d, cap of s10 => %d, s11 =>%d\n", len(s10), cap(s10), s10)
+
+	// the same, new cap is 18
+	s11 := append(s7, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22)
+	fmt.Printf("len of s11 => %d, cap of s11 => %d, s11 =>%d\n\n", len(s11), cap(s11), s11)
+
+	// trap of append
+	array1 := []int{10, 11, 12, 13, 14}
+	slice1 := array1[1:4]
+	fmt.Printf("address of array1 => %p, address of array1[0] => %p, array is =>%d\n", &array1, &array1[0], array1)
+	// slice1 refers to : also can using %p => &slice1[0]
+	fmt.Printf("slice1 refers to(address of actual array, slice[0] now refers to array1[1]) => %p, slice1 is => %d\n", &slice1[0], slice1)
+	fmt.Printf("address of slice 1 (address of slice struct) => %p\n", &slice1)
+
+	array1[1] += 100 // change array1[1], slice1[0], because slice1 refers to array1
+	slice1[1] += 200 // change array1[2], slice1[1], because slice1[1] refers to array1[2]
+	fmt.Println("\nafter array1[0] += 100 and slice1[1] += 200")
+	fmt.Printf("address of array1 => %p, array is => %d\n", array1, array1)
+	// slice1 refers to : also can using %p => &slice1[0]
+	fmt.Printf("slice1 refers to(address of actual array, slice[0] now refers to array1[1]) => %p, slice1 is =>%d\n", slice1, slice1)
+	fmt.Printf("address of slice 1 (address of slice struct) => %p\n", &slice1)
+
+	/*
+		2 ways to call append:
+		one is append(s, element)
+		another is append(s1, s2...), notice ... is every important.
+	*/
+	slice2 := append(slice1, s11...)
+	fmt.Println("\nafter 'slice2 := append(slice1, s11...)'")
+	fmt.Printf("address of array1 => %p, array is => %d\n", array1, array1)
+	// slice1 refers to : also can using %p => &slice1[0]
+	fmt.Printf("slice1 refers to(address of actual array, slice[0] now refers to array1[1]) => %p, slice1 is => %d\n", slice1, slice1)
+	fmt.Printf("address of slice 1 (address of slice struct) => %p\n", &slice1)
+	fmt.Printf("slice2 refers to(address of new actual array) => %p, slice2 is => %d\n", &slice2[0], slice2)
+	fmt.Printf("slice2 refers to(address of new actual array) => %p, slice2 is => %d\n", slice2, slice2)
+	fmt.Printf("address of slice 2 (address of slice struct) => %p\n", &slice2)
+	// verify the Dilatancy principle again, cap of slice2 should be 20.
+	fmt.Printf("len of slice2 => %d, cap of slice2 => %d, slice2 => %d\n\n", len(slice2), cap(slice2), slice2)
+
 }
