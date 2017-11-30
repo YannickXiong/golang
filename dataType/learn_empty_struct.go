1528140e25136ab19f9309b77ced1a587c83b559
YannickXiong <xiong.yannick@student.oaklandcc.edu>
2017-11-30 21:15:31 +0800

    modified:   learn_empty_struct.go

--- dataType/learn_empty_struct.go
+++ dataType/learn_empty_struct.go
@@ -18,8 +18,8 @@ type IAVPType interface {
 }
 
 type AVPTypeDesc struct {
-	name     string
-	dataType IAVPType
+	name     string   // size 16
+	dataType IAVPType // size 16
 }
 
 type AVPType struct{}
@@ -39,15 +39,26 @@ func (a AVPType) String() string {
 func LearnEmptyStruct() {
 	avpDesc := [2]AVPTypeDesc{
 
-		0: {"username", AVPType{}}, //ok
-		1: {"username", avpTypeT},  //ok, is the same to {"username", AVPType{}}, avpDesc => [{username {}} {username {}}]
+		// if AVPType does not implement IAVPType interface, then cannot use AVPType as an interface
+		// in field value. Errors is:
+		// cannot use AVPType literal (type AVPType) as type IAVPType in field value:
+		// AVPType does not implement IAVPType (missing String method)
+		0: {"id", AVPType{}}, //ok
+		1: {"id", avpTypeT},  //ok, is the same to {"username", AVPType{}}, avpDesc => [{username {}} {username {}}]
 		// 0: {"username", AVPType}, // not ok
-		// 0: {"username", new(AVPType)}, //ok,  
-		avpDesc => [{username 0x116ccf0}]
+		// 0: {"username", new(AVPType)}, //ok,  avpDesc => [{username 0x116ccf0}]
 		// 0: {"username", *new(AVPType)}, //ok, avpDesc => [{username {}} {username {}}]
 
 	}
 
 	fmt.Printf("avpDesc => %v\n", avpDesc)
 	fmt.Printf("size : => %d\n", unsafe.Sizeof(avpDesc))
+
+	var a struct{}
+	var b struct{}
+
+	// empty struct has 0 size, and the same address!
+	fmt.Printf("a size : => %d, addr => %p\n", unsafe.Sizeof(a), &a)
+	fmt.Printf("b size : => %d, addr => %p\n", unsafe.Sizeof(b), &b)
+	fmt.Println("a == b ? => ", a == b)
 }
